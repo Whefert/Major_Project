533ee4a1ced5393fedaa9b1a3db62f86c9980562
Whefert <jeffersondaley@gmail.com>
2023-04-13 11:46:50 +0100

    Initial commit

---
+++ app/src/main/java/com/example/majorproject/CourseEntity.kt
@@ -0,0 +1,9 @@
+package com.example.majorproject
+
+data class CourseEntity(
+    val code:String,
+    val name:String,
+    val credits: String,
+    val prerequisiteOne:String,
+    val prerequisiteTwo: String
+)
---
+++ app/src/main/java/com/example/majorproject/FacultyStaffDirectory.kt
@@ -0,0 +1,11 @@
+package com.example.majorproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FacultyStaffDirectory : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_faculty_staff_directory)
+    }
+}
